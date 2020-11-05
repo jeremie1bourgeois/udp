a32aad7ef64f8a7eb7a4e0ca5fc346357dabfb9d
Thomas-Raveneau <thomas.raveneau@epitech.eu>
2020-11-05 23:49:11 +0100

    [ADD] - menfou2

--- UdpServer.cpp
+++ UdpServer.cpp
@@ -22,12 +22,10 @@ UdpServer::~UdpServer()
 {
 }
 
-const PackageUdp UdpServer::makePackage(int i, std::string str) const noexcept
+void UdpServer::makePackage(int i, std::string str)
 {
-    return PackageUdp {
-        i,
-        str
-    };
+    _toSend.i = i;
+    _toSend.str = str;
 }
 
 std::string make_daytime_string()
@@ -41,36 +39,50 @@ void UdpServer::start_receive()
 {
     _socket.async_receive_from(
         boost::asio::buffer(_recvBuffer), _remoteEndpoint,
-        boost::bind(&UdpServer::handle_receive, this,
-        boost::asio::placeholders::error,
-        boost::asio::placeholders::bytes_transferred));
+        [this](boost::system::error_code e, size_t length)
+            {
+                makePackage(1, "OUAIS YOLO");
+                ostringstream archive_stream;
+                boost::archive::text_oarchive arc(archive_stream);
+                arc << _toSend;
+
+                _socket.async_send_to(boost::asio::buffer(archive_stream.str()), _remoteEndpoint,
+                    [this](boost::system::error_code e, size_t length)
+                    {
+
+                    }
+                );
+
+                start_receive();
+            }
+        );
 }
 
-void UdpServer::handle_receive(const boost::system::error_code& error, std::size_t)
-{
-    if (!error)
-    {
-//        boost::shared_ptr<std::string> message(new std::string(make_daytime_string()));
-//        boost::shared_ptr<Pack> send(new Pack(makePackage()));
-        PackageUdp test = makePackage(1, "salut");
-        ostringstream archive_stream;
-        boost::archive::text_oarchive arc(archive_stream);
-        arc << test;
-        // char buffer[sizeof(Pack)];
-        // memcpy(buffer, &_toSend, sizeof(Pack));
+// void UdpServer::handle_receive(const boost::system::error_code& error, std::size_t)
+// {
+//     if (!error)
+//     {
+// //        boost::shared_ptr<std::string> message(new std::string(make_daytime_string()));
+// //        boost::shared_ptr<Pack> send(new Pack(makePackage()));
+//         makePackage(1, "salut");
+//         ostringstream archive_stream;
+//         boost::archive::text_oarchive arc(archive_stream);
+//         arc << _toSend;
+//         // char buffer[sizeof(Pack)];
+//         // memcpy(buffer, &_toSend, sizeof(Pack));
 
-//        boost::shared_ptr<std::string> message(new std::string(buffer));
+// //        boost::shared_ptr<std::string> message(new std::string(buffer));
 
-        _socket.async_send_to(boost::asio::buffer(archive_stream.str()), _remoteEndpoint,
-            boost::bind(&UdpServer::handle_send, this, archive_stream.str(),
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+//         _socket.async_send_to(boost::asio::buffer(archive_stream.str()), _remoteEndpoint,
+//             boost::bind(&UdpServer::handle_send, this, archive_stream.str(),
+//             boost::asio::placeholders::error,
+//             boost::asio::placeholders::bytes_transferred));
 
-        start_receive();
-    }
-}
+//         start_receive();
+//     }
+// }
 
-void UdpServer::handle_send(boost::shared_ptr<std::string>, const boost::system::error_code&, std::size_t)
-{
+// void UdpServer::handle_send(boost::shared_ptr<std::string>, const boost::system::error_code&, std::size_t)
+// {
 
-}
+// }
